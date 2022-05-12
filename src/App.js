9bfb2c2eca40da1b849cc9653967f0b0ea509c38
githubleonch000333 <github100663093+leonch000333@users.noreply.github.com>
2022-05-12 15:42:14 +0900

    main master確認

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Button>arigato</Button>
+    <Box>a</Box>
   );
 }
 
